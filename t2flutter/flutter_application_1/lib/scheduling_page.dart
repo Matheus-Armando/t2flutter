d29e6bc88c9bc6029fa1fa43c9d3fcbf6ade5d8f
Matheus-Armando <matheuschioquetta1@gmail.com>
2024-05-13 12:06:22 -0300

    correção schedule

--- t2flutter/flutter_application_1/lib/scheduling_page.dart
+++ t2flutter/flutter_application_1/lib/scheduling_page.dart
@@ -7,9 +7,19 @@ class SchedulingPage extends StatefulWidget {
 
 class _SchedulingPageState extends State<SchedulingPage> {
   List<String> availableTimes = [
-    '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00'
+    '09:00',
+    '10:00',
+    '11:00',
+    '12:00',
+    '13:00',
+    '14:00',
+    '15:00',
+    '16:00',
+    '17:00'
   ];
   DateTime? selectedDate;
+  String? patientName;
+  List<Map<String, dynamic>> scheduledAppointments = [];
 
   @override
   Widget build(BuildContext context) {
@@ -35,7 +45,7 @@ class _SchedulingPageState extends State<SchedulingPage> {
               }
             },
           ),
-          if (selectedDate == null) 
+          if (selectedDate == null)
             Expanded(
               child: Center(
                 child: Text('Por favor, selecione uma data.'),
@@ -47,30 +57,87 @@ class _SchedulingPageState extends State<SchedulingPage> {
                 itemCount: availableTimes.length,
                 itemBuilder: (context, index) {
                   return ListTile(
-                    title: Text('${selectedDate?.toIso8601String().substring(0, 10)} ${availableTimes[index]}'),
+                    title: Text(
+                        '${selectedDate?.toIso8601String().substring(0, 10)} ${availableTimes[index]}'),
                     trailing: Icon(Icons.check_circle, color: Colors.green),
                     onTap: () async {
-                      bool confirm = await showDialog(
-                        context: context,
-                        builder: (context) => AlertDialog(
-                          title: Text('Confirmar Agendamento'),
-                          content: Text('Você quer agendar uma consulta para ${selectedDate?.toIso8601String().substring(0, 10)} ${availableTimes[index]}?'),
-                          actions: <Widget>[                
-                            TextButton(
-                              child: Text('Cancelar'),
-                              onPressed: () => Navigator.of(context).pop(false),
-                            ),
-                            TextButton(
-                              child: Text('Confirmar'),
-                              onPressed: () => Navigator.of(context).pop(true),
-                            ),
-                          ],
-                        ),
+                      String time = availableTimes[index];
+                      Map<String, dynamic> appointment =
+                          scheduledAppointments.firstWhere(
+                        (appointment) =>
+                            appointment['time'] == time &&
+                            appointment['date'] ==
+                                selectedDate
+                                    ?.toIso8601String()
+                                    .substring(0, 10),
+                        orElse: () => <String,
+                            dynamic>{}, // Return an empty map instead of null
                       );
-                      if (confirm) {
-                        setState(() {
-                          availableTimes.removeAt(index);
-                        });
+
+                      if (appointment.isNotEmpty) {
+                        // Check if the map is not empty
+                        // This time is already scheduled. Show the patient's name and do nothing else.
+                        showDialog(
+                          context: context,
+                          builder: (context) => AlertDialog(
+                            title: Text('Horário Agendado'),
+                            content: Text(
+                                'Este horário já foi agendado para ${appointment['patientName']}.'),
+                            actions: <Widget>[
+                              TextButton(
+                                child: Text('OK'),
+                                onPressed: () => Navigator.of(context).pop(),
+                              ),
+                            ],
+                          ),
+                        );
+                      } else {
+                        // This time is not scheduled yet. Show the scheduling dialog.
+                        TextEditingController _controller =
+                            TextEditingController();
+                        bool confirm = await showDialog(
+                          context: context,
+                          builder: (context) => AlertDialog(
+                            title: Text('Confirmar Agendamento'),
+                            content: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              children: <Widget>[
+                                Text(
+                                    'Você quer agendar uma consulta para ${selectedDate?.toIso8601String().substring(0, 10)} $time?'),
+                                TextField(
+                                  controller: _controller,
+                                  decoration: InputDecoration(
+                                    labelText: 'Nome do Paciente',
+                                  ),
+                                ),
+                              ],
+                            ),
+                            actions: <Widget>[
+                              TextButton(
+                                child: Text('Cancelar'),
+                                onPressed: () =>
+                                    Navigator.of(context).pop(false),
+                              ),
+                              TextButton(
+                                child: Text('Confirmar'),
+                                onPressed: () {
+                                  String patientName = _controller.text;
+                                  Navigator.of(context).pop(patientName
+                                      .isNotEmpty); // Confirm only if patient name is not empty
+                                  if (patientName.isNotEmpty) {
+                                    scheduledAppointments.add({
+                                      'time': time,
+                                      'date': selectedDate
+                                          ?.toIso8601String()
+                                          .substring(0, 10),
+                                      'patientName': patientName,
+                                    });
+                                  }
+                                },
+                              ),
+                            ],
+                          ),
+                        );
                       }
                     },
                   );

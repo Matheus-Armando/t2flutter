f5577918ff4bba7f71ad118bc64ed6ca1e075417
Roger Jacobsen <rogerjacobsencj@gmail.com>
2024-05-12 21:27:56 -0300

    Calendário

--- t2flutter/flutter_application_1/lib/scheduling_page.dart
+++ t2flutter/flutter_application_1/lib/scheduling_page.dart
@@ -9,6 +9,7 @@ class _SchedulingPageState extends State<SchedulingPage> {
   List<String> availableTimes = [
     '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00'
   ];
+  DateTime? selectedDate;
 
   @override
   Widget build(BuildContext context) {
@@ -16,38 +17,67 @@ class _SchedulingPageState extends State<SchedulingPage> {
       appBar: AppBar(
         title: Text('Agendamento de Consultas'),
       ),
-      body: ListView.builder(
-        itemCount: availableTimes.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(availableTimes[index]),
-            trailing: Icon(Icons.check_circle, color: Colors.green),
-            onTap: () async {
-              bool confirm = await showDialog(
+      body: Column(
+        children: [
+          ElevatedButton(
+            child: Text('Escolha uma data'),
+            onPressed: () async {
+              final date = await showDatePicker(
                 context: context,
-                builder: (context) => AlertDialog(
-                  title: Text('Confirmar Agendamento'),
-                  content: Text('Você quer agendar uma consulta para ${availableTimes[index]}?'),
-                  actions: <Widget>[                
-                    TextButton(
-                      child: Text('Cancelar'),
-                      onPressed: () => Navigator.of(context).pop(false),
-                    ),
-                    TextButton(
-                      child: Text('Confirmar'),
-                      onPressed: () => Navigator.of(context).pop(true),
-                    ),
-                  ],
-                ),
+                initialDate: DateTime.now(),
+                firstDate: DateTime.now(),
+                lastDate: DateTime(DateTime.now().year + 1),
               );
-              if (confirm) {
+              if (date != null) {
                 setState(() {
-                  availableTimes.removeAt(index);
+                  selectedDate = date;
                 });
               }
             },
-          );
-        },
+          ),
+          if (selectedDate == null) 
+            Expanded(
+              child: Center(
+                child: Text('Por favor, selecione uma data.'),
+              ),
+            )
+          else
+            Expanded(
+              child: ListView.builder(
+                itemCount: availableTimes.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text('${selectedDate?.toIso8601String().substring(0, 10)} ${availableTimes[index]}'),
+                    trailing: Icon(Icons.check_circle, color: Colors.green),
+                    onTap: () async {
+                      bool confirm = await showDialog(
+                        context: context,
+                        builder: (context) => AlertDialog(
+                          title: Text('Confirmar Agendamento'),
+                          content: Text('Você quer agendar uma consulta para ${selectedDate?.toIso8601String().substring(0, 10)} ${availableTimes[index]}?'),
+                          actions: <Widget>[                
+                            TextButton(
+                              child: Text('Cancelar'),
+                              onPressed: () => Navigator.of(context).pop(false),
+                            ),
+                            TextButton(
+                              child: Text('Confirmar'),
+                              onPressed: () => Navigator.of(context).pop(true),
+                            ),
+                          ],
+                        ),
+                      );
+                      if (confirm) {
+                        setState(() {
+                          availableTimes.removeAt(index);
+                        });
+                      }
+                    },
+                  );
+                },
+              ),
+            ),
+        ],
       ),
     );
   }

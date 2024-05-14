1535fa1555049e91848bf7f9b452accfcf322e9e
Matheus-Armando <141080917+Matheus-Armando@users.noreply.github.com>
2024-05-14 00:40:54 -0300

    estilização do nome e remoção de comentarios

--- t2flutter/flutter_application_1/lib/doctor_profile_page.dart
+++ t2flutter/flutter_application_1/lib/doctor_profile_page.dart
@@ -9,7 +9,13 @@ class User {
   final String specialty;
   final String description;
 
-  User({required this.username, required this.password, required this.name, required this.email, required this.specialty, required this.description});
+  User(
+      {required this.username,
+      required this.password,
+      required this.name,
+      required this.email,
+      required this.specialty,
+      required this.description});
 }
 
 class DoctorProfilePage extends StatefulWidget {
@@ -21,30 +27,38 @@ class _DoctorProfilePageState extends State<DoctorProfilePage> {
   String username = '';
   User? userInfo;
 
-  // Mapa de exemplo de nomes de usuários para endereços de e-mail
   Map<String, String> userEmails = {
     'matheus': 'matheus@exemplo.com',
     'roger': 'roger@exemplo.com',
   };
 
-  // Mapa de exemplo de nomes de usuários para especialidades
   Map<String, String> userSpecialties = {
     'matheus': 'Cardiologista',
     'roger': 'Neurologista',
   };
 
-  // Mapa de exemplo de nomes de usuários para descrições
   Map<String, String> userDescriptions = {
-    'matheus': 'Dr. Matheus é um cardiologista com mais de 10 anos de experiência.',
-    'roger': 'Dr. Roger é um neurologista renomado com vasta experiência em seu campo.',
+    'matheus':
+        'Dr. Matheus é um cardiologista com mais de 10 anos de experiência.',
+    'roger':
+        'Dr. Roger é um neurologista renomado com vasta experiência em seu campo.',
   };
 
   Future<User> getUserInfo(String username) async {
-    // Substitua isso pelo seu próprio mecanismo de recuperação de informações do usuário
     String email = userEmails[username.toLowerCase()] ?? 'unknown@exemplo.com';
-    String specialty = userSpecialties[username.toLowerCase()] ?? 'Desconhecida';
-    String description = userDescriptions[username.toLowerCase()] ?? 'Descrição não disponível';
-    return Future.delayed(Duration(seconds: 1), () => User(username: username, password: '123', name: 'Nome do Usuário', email: email, specialty: specialty, description: description));
+    String specialty =
+        userSpecialties[username.toLowerCase()] ?? 'Desconhecida';
+    String description =
+        userDescriptions[username.toLowerCase()] ?? 'Descrição não disponível';
+    return Future.delayed(
+        Duration(seconds: 1),
+        () => User(
+            username: username,
+            password: '123',
+            name: 'Nome do Usuário',
+            email: email,
+            specialty: specialty,
+            description: description));
   }
 
   Future<String> getUsername() async {
@@ -58,8 +72,8 @@ class _DoctorProfilePageState extends State<DoctorProfilePage> {
     getUsername().then((value) => setState(() {
           username = value;
           getUserInfo(username).then((user) => setState(() {
-            userInfo = user;
-          }));
+                userInfo = user;
+              }));
         }));
   }
 
@@ -69,38 +83,49 @@ class _DoctorProfilePageState extends State<DoctorProfilePage> {
       appBar: AppBar(
         title: Text('Perfil do Médico'),
       ),
-      body: userInfo == null 
-        ? Center(child: CircularProgressIndicator()) 
-        : Container(
-            width: MediaQuery.of(context).size.width * 1.0, // 70% da largura da tela
-            height: MediaQuery.of(context).size.height * 1.0, // 70% da altura da tela
-            child: Padding(
-              padding: EdgeInsets.all(20),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Olá, ${userInfo!.username}, bem vindo ao seu perfil!',
-                    style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
+      body: userInfo == null
+          ? Center(child: CircularProgressIndicator())
+          : Container(
+              width: MediaQuery.of(context).size.width *
+                  1.0, 
+              height: MediaQuery.of(context).size.height *
+                  1.0, 
+              child: Padding(
+                padding: EdgeInsets.all(20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    RichText(
+                      text: TextSpan(
+                        style: DefaultTextStyle.of(context).style.copyWith(decoration: TextDecoration.none),
+                        children: <TextSpan>[
+                          TextSpan(text: 'Olá ', style: TextStyle(color: Colors.black)),
+                          TextSpan(
+                              text: '${userInfo!.username}',
+                              style: TextStyle(color: Colors.purple)),
+                          TextSpan(text: ', bem vindo ao seu perfil!', style: TextStyle(color: Colors.black)),
+                        ],
+                      ),
                     ),
-                  ),
-                  SizedBox(height: 5),
-                  Text('Nome: ${userInfo!.username}', style: TextStyle(fontSize: 18)),
-                  Divider(thickness: 0.5),
-                  SizedBox(height: 5),
-                  Text('Email: ${userInfo!.email}', style: TextStyle(fontSize: 18)),
-                  Divider(thickness: 0.5),
-                  SizedBox(height: 5),
-                  Text('Especialidade: ${userInfo!.specialty}', style: TextStyle(fontSize: 18)),
-                  Divider(thickness: 0.5),
-                  SizedBox(height: 5),
-                  Text('Descrição: ${userInfo!.description}', style: TextStyle(fontSize: 18)),
-                ],
+                    SizedBox(height: 5),
+                    Text('Nome: ${userInfo!.username}',
+                        style: TextStyle(fontSize: 18)),
+                    Divider(thickness: 0.5),
+                    SizedBox(height: 5),
+                    Text('Email: ${userInfo!.email}',
+                        style: TextStyle(fontSize: 18)),
+                    Divider(thickness: 0.5),
+                    SizedBox(height: 5),
+                    Text('Especialidade: ${userInfo!.specialty}',
+                        style: TextStyle(fontSize: 18)),
+                    Divider(thickness: 0.5),
+                    SizedBox(height: 5),
+                    Text('Descrição: ${userInfo!.description}',
+                        style: TextStyle(fontSize: 18)),
+                  ],
+                ),
               ),
             ),
-          ),
     );
   }
 }

3e3d2caa0c56132e24ef83bd551655493273e933
Matheus-Armando <141080917+Matheus-Armando@users.noreply.github.com>
2024-05-14 00:53:35 -0300

    ajustes finos visuais

--- t2flutter/flutter_application_1/lib/home_page.dart
+++ t2flutter/flutter_application_1/lib/home_page.dart
@@ -26,7 +26,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home Page'),
+        title: Icon(Icons.home),
         automaticallyImplyLeading: false,
         actions: <Widget>[
           IconButton(
